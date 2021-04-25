278be77215a0a1286e106fcb578500709fc59581
guy <guyklinger94@gmail.com>
2021-04-25 11:34:24 +0300

    background image

--- app.js
+++ app.js
@@ -396,6 +396,7 @@ function generateRandomGame(){
   document.getElementById("favcolor2").value = randomColorPicker()
   document.getElementById("favcolor3").value = randomColorPicker()
 
+  keys = {left:37, up:38, right:39, down:40};
   document.getElementById("kbd-up").innerHTML = "↑";
   document.getElementById("kbd-down").innerHTML = "↓";
   document.getElementById("kbd-left").innerHTML = "←";;
@@ -797,60 +798,3 @@ function UpdatePosition() {
     resetGame()
   }
 }
-
-// function validateSignUp(e) {
-//   e.preventDefault();
-
-//   let userName = $("#uname").val();
-//   let fullName = $("#fname").val();
-//   let email = $("#email").val();
-//   let password = $("#pword").val();
-//   let repeatPassWord = $("#repeatPword").val();
-
-//   const passwordValidation = new RegExp("(?=.*[0-9])(?=.*[a-zA-Z]).{6,}");
-//   const nameValidation = new RegExp("![^a-zA-Z]");
-//   const emailValidation = new RegExp("[S+@S+.S+]");
-
-//   let names = fullName.split(" ");
-//   let numOfValidations = 5;
-
-//   $(".signup").filter(function () {
-//     return $.trim($(this).val()).length == 0;
-//   }).length == 0
-//     ? numOfValidations--
-//     : null;
-
-//   passwordValidation.test(password) ? numOfValidations-- : null;
-//   emailValidation.test(email) ? numOfValidations-- : null;
-//   password === repeatPassWord ? numOfValidations-- : null;
-
-//   const numberInName = names.filter((item) => nameValidation.test(item));
-//   numberInName.length == 0 ? numOfValidations-- : null;
-
-//   if (numOfValidations != 0) {
-//     timeOutAlert("form is not defined well.");
-//   } else {
-//     usersMap[userName] = password;
-//     $("a[href='#welcome']").click();
-//   }
-// }
-
-// function loginUser(e) {
-//   e.preventDefault();
-
-//   let loginUserName = $("#loginUserName").val();
-//   let loginPassword = $("#loginPassword").val();
-
-//   if (isLoggedIn == true) {
-//     timeOutAlert("A user is already logged in.");
-//   } 
-//   else if ( loginUserName in usersMap &&
-//             usersMap[loginUserName] == loginPassword) {
-//     isLoggedIn = true;
-//     UserName = loginUserName;
-//     $("a[href='#settings']").click();
-//   } 
-//   else {
-//     timeOutAlert("Details are wrong. Try again or register.");
-//   }
-// }
